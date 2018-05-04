3240f1febb889d4a74cea0221dbd7b3128593db6
lalolanda1221 <38925129+lalolanda1221@users.noreply.github.com>
2018-05-04 12:23:48 -0500

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.er778700.actividad1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Eduardo Rodriguez Romero No. Cuenta 410048980");
+    }
+}
